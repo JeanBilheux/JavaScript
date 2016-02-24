ce3eb4d1cda5f8a6c0d77410c7a91c078a80b540
JeanBilheux <bilheuxjm@ornl.gov>
2016-02-23 21:53:45 -0500

    started to implement removal of tags

--- HeadFirst/chap04/begin/scripts/my_scripts.js
+++ HeadFirst/chap04/begin/scripts/my_scripts.js
@@ -1,5 +1,24 @@
-
 $(document).ready(function(){
 
+    var v = false;
+    
+    $("button#vegOn").click(function() {
+        if (v == false) {
+            
+            $(".meat").detach();
+                
+            v = true;
+        };     
+    }); //end of button vegOn
+    
+    $("button#restoreMe").click(function() {
+        if (v == true) {
+            
+
+            
+            v = false;
+        }
+        
+    }) // end of button restoreMe
 	
 });
