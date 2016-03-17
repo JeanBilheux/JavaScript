35923a62c3db63b0964ab2c9adc19eb34ecb1b76
JeanBilheux <bilheuxjm@ornl.gov>
2016-03-16 21:11:11 -0400

    Fixed size of buttons

--- RayanCheckListAMPM/scripts/my_scripts.js
+++ RayanCheckListAMPM/scripts/my_scripts.js
@@ -4,4 +4,12 @@ $(document).ready(function(){
       $(this).css({"background-color": "green"})
   })
 	
+var h = window.innerHeight;
+var w = window.innerWidth;
+    
+var number_button = $(".button").length;
+    
+$(".button").css({"height": (h/number_button)*0.95,
+                 "width": w*0.95});    
+  
 });
